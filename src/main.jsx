df243f3b9ba9e35453b34caf1dc19d5fc8af9993
Mrlegend08 <muslimdasturchi@gmail.com>
2023-08-06 16:43:45 +0500

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Root from './Root'
-import { Provider as ApiProvider } from './context/ApiContext'
+import { Provider as ApiProvider } from './context/apiContext'
 import { Provider as SimpleProvider } from './context/simpleContext'
 import { Provider as SimpleApiProvider } from './context/simpleApiContext'
 import { BrowserRouter } from "react-router-dom"
